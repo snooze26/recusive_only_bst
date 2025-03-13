4e1253bc644b67e902991cc40aef184570832df0
snooze26 <langdon.douglas26@gmail.com>
2025-03-12 20:13:05 -0500

    added some text to psuedo files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { Node } from "./node.js";
 import { Tree } from "./binarayTreeClass.js";
+import { treeOperations } from "./treeOperations.js";
 
 const node0 = new Node(0);
 const node1 = new Node(1);
@@ -9,6 +10,7 @@ const node4 = new Node(4);
 const node5 = new Node(5); 
 const node6 = new Node(6); 
 const node7 = new Node(7); 
+const node8 = new Node(8); 
 
 
 const testArray = [node0, node1, node2, node3,
@@ -16,4 +18,9 @@ const testArray = [node0, node1, node2, node3,
 ]
 
 const testTree = new Tree(testArray); 
-console.log(testTree);
+
+const treeOps = new treeOperations(testTree); 
+
+treeOps.insert(node8); 
+
+console.log(JSON.stringify(testTree.root));
