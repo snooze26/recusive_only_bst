e3217f833297e61349b6563043ad21deecdcdf72
snooze26 <langdon.douglas26@gmail.com>
2025-03-12 19:26:37 -0500

    added buildTree function and tested it

--- binarayTreeClass.js
+++ binarayTreeClass.js
@@ -1,16 +1,20 @@
+import { Node } from "./node.js";
+
 export class Tree { 
     constructor(array) { 
         this.array = array;
-        // this.root = //buildTree function
+        this.root = this.buildTree(this.array, 0, this.array.length-1);
     }
 
     buildTree(array, start, end) { 
         if(start > end) return null; 
-
+        
         let mid = Math.floor((start + end) / 2); 
         let root = array[mid]; 
-
+        
         root.left = this.buildTree(array, start, mid - 1);
         root.right = this.buildTree(array, mid + 1, end);
+
+        return root
     }
 }
---
+++ index.js
@@ -0,0 +1,19 @@
+import { Node } from "./node.js";
+import { Tree } from "./binarayTreeClass.js";
+
+const node0 = new Node(0);
+const node1 = new Node(1);
+const node2 = new Node(2); 
+const node3 = new Node(3); 
+const node4 = new Node(4); 
+const node5 = new Node(5); 
+const node6 = new Node(6); 
+const node7 = new Node(7); 
+
+
+const testArray = [node0, node1, node2, node3,
+                   node4, node5, node6, node7
+]
+
+const testTree = new Tree(testArray); 
+console.log(testTree);
