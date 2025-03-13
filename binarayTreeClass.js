813957b8f64629fd2d81b6cc8ac1766b1c8ef015
snooze26 <langdon.douglas26@gmail.com>
2025-03-12 20:06:41 -0500

    turn root into a node

--- binarayTreeClass.js
+++ binarayTreeClass.js
@@ -10,7 +10,7 @@ export class Tree {
         if(start > end) return null; 
         
         let mid = Math.floor((start + end) / 2); 
-        let root = array[mid]; 
+        let root = new Node(array[mid]); 
         
         root.left = this.buildTree(array, start, mid - 1);
         root.right = this.buildTree(array, mid + 1, end);
