5247d49c735d467854de36e8d16aee05be4b9b11
snooze26 <langdon.douglas26@gmail.com>
2025-03-12 19:06:38 -0500

    create bst and buildTree function

--- binarayTreeClass.js
+++ binarayTreeClass.js
@@ -1,6 +1,16 @@
 export class Tree { 
-    constructors(array) { 
+    constructor(array) { 
         this.array = array;
         // this.root = //buildTree function
+    }
+
+    buildTree(array, start, end) { 
+        if(start > end) return null; 
+
+        let mid = Math.floor((start + end) / 2); 
+        let root = array[mid]; 
+
+        root.left = this.buildTree(array, start, mid - 1);
+        root.right = this.buildTree(array, mid + 1, end);
     }
 }
